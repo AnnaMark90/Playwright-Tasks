9f45557a844fe152309018845c3e34fdc79f1aaf
Anna Markovskaya <109307073+AnnaMark90@users.noreply.github.com>
2025-11-20 17:18:35 +0300

    Add types, start functions

--- form_task/tests/example.spec.ts
+++ form_task/tests/example.spec.ts
@@ -1,18 +1,43 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, type Page} from '@playwright/test';
+import type { userData } from '../types/types';
+import { formSelectors } from '../types/types';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const validData: userData = {
+  firstName: 'Alex',
+  secondName: 'Ivanov',
+  email: 'ivanov@mail.com',
+  gender: 'Male', 
+  mobile: '2912345678', 
+  birthDate: '2000-05-15', 
+  subjects: ['Math', 'Physics', 'English'], 
+  hobbies: ['Sports', 'Reading'], 
+  picture: './images/image.png', 
+  currentAddress: 'ул. Ленина, д. 10, Минск',
+  stateCity: {
+    state: 'NCR',
+    city: 'Delhi',
+  }
+};
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+async function openPageDemoqa (page: Page): Promise<void>{ 
+  await page.goto('https://demoqa.com/text-box',  { timeout: 60000 });
+  await expect(page.getByRole('heading', { name: 'Practice Form' })).toBeVisible();
+}
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+async function submitForm(page: Page): Promise<void>{
+  await page.getByRole('button', { name: 'Submit' }).click();
+}
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+async function fillTheData(page: Page, data: userData) {
+  const fields = ['firstName', 'secondName', 'email', 'mobile', 'currentAddress'] as const; // массив в литерал
+  for (const field of fields) { 
+    await page.fill(formSelectors[field], data[field]);
+  }
+  
+}
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('fill in valid data', async ({ page }) => {
+  await openPageDemoqa(page);
+  await fillTheData(page, validData);
+  await submitForm(page);
 });
