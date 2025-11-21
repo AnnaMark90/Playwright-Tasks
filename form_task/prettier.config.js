72ec3de3ffd7c514c7c2ea5cfb06ef6187f0b2cc
Anna Markovskaya <109307073+AnnaMark90@users.noreply.github.com>
2025-11-21 12:07:46 +0300

    Add changes

---
+++ form_task/prettier.config.js
@@ -0,0 +1,13 @@
+export default {
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    quoteProps: 'consistent',
+    trailingComma: 'all',
+    bracketSpacing: true,
+    bracketSameLine: true,
+    arrowParens: 'always',
+    endOfLine: 'lf',
+};
--- form_task/types/types.ts
+++ form_task/types/types.ts
@@ -1,34 +1,34 @@
 export type Gender = 'Male' | 'Female' | 'Other';
 export type Hobby = 'Sports' | 'Reading' | 'Music';
 
-export interface  userData {
-  firstName: string,
-  secondName: string,
-  gender: Gender,
-  email: string,
-  mobile: string, 
-  birthDate: string, // формат YYYY-MM-DD
-  subjects: string[],
-  hobbies: Hobby[],
-  picture: string, // путь к локальному файлу
-  currentAddress: string,
-  stateCity: {
-    state: string,
-    city: string,
-  }, 
-};
+export interface userData {
+    firstName: string;
+    lastName: string;
+    gender: Gender;
+    email: string;
+    mobile: string;
+    birthDate: string; // формат YYYY-MM-DD
+    subjects: string[];
+    hobbies: Hobby[];
+    picture: string; // путь к локальному файлу
+    currentAddress: string;
+    stateCity: {
+        state: string;
+        city: string;
+    };
+}
 
 export const formSelectors = {
-  firstName: '#firstName',
-  secondName: '#lastName',
-  email: '#userEmail',
-  mobile: '#userNumber',
-  currentAddress: '#currentAddress',
-  gender: (value: string) => `input[name="gender"][value="${value}"]`,
-  birthDate: '#dateOfBirthInput',
-  subjectsInput: '#subjectsInput',
-  hobbies: (label: string) => `label:has-text("${label}")`,
-  picture: '#uploadPicture',
-  state: '#state',
-  city: '#city',
+    firstName: '#firstName',
+    lastName: '#lastName',
+    email: '#userEmail',
+    mobile: '#userNumber',
+    currentAddress: '#currentAddress',
+    gender: (value: string) => `input[name="gender"][value="${value}"]`,
+    birthDate: '#dateOfBirthInput',
+    subjectsInput: '#subjectsInput',
+    hobbies: (label: string) => `label:has-text("${label}")`,
+    picture: '#uploadPicture',
+    state: '#state',
+    city: '#city',
 };
