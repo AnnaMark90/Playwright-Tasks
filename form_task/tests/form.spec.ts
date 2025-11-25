0f39b69fa51617110989dfbe81f16cc2c1df7afb
Anna Markovskaya <109307073+AnnaMark90@users.noreply.github.com>
2025-11-25 10:46:00 +0300

    Adding negative scenarios

--- form_task/tests/form.spec.ts
+++ form_task/tests/form.spec.ts
@@ -1,14 +1,19 @@
 import { test, expect, type Page } from "@playwright/test";
-import type { userData } from "../types/types";
-import { formSelectors, validData } from "../types/types";
+import { formSelectors, validData, TestDataInvalEmail } from "../types/types";
 import {
   openPageDemoqa,
-  fillTextData,
+  fillAllTextData,
   chooseOption,
   chooseDateOfBirth,
   selectLocation,
   submitForm,
   uploadImage,
+  expectTableValues,
+  showModal,
+  closeModal,
+  fillPartialTextData,
+  invalidHighlight,
+  expectTableOneValue,
 } from "../helpers/helpers";
 
 test.describe("testing form", () => {
@@ -18,19 +23,99 @@ test.describe("testing form", () => {
     await openPageDemoqa(page);
   });
 
-  test.afterEach(async ({ page }) => {
-    await submitForm(page);
-  });
+  test("filling in valid data", async ({ page }) => {
+    await fillAllTextData(page, validData);
 
-  test("fill in valid data", async ({ page }) => {
-    await fillTextData(page, validData);
-
-    await chooseOption(page, "subject", [...validData.subjects]);
     await chooseOption(page, "gender", [validData.gender]);
-    await chooseOption(page, "hobbies", [...validData.hobbies]);
+    // await chooseOption(page, "subject", [...validData.subjects]);
+    // await chooseOption(page, "hobbies", [...validData.hobbies]);
 
     await chooseDateOfBirth(page, validData.birthDate);
     await uploadImage(page, validData.picture);
     await selectLocation(page, validData.stateCity);
+
+    await submitForm(page);
+
+    await showModal(page);
+    await expectTableValues(page, validData);
+    await closeModal(page);
+  });
+
+  test("filling in data without obligatory ones", async ({ page }) => {
+    await fillPartialTextData(page, validData, ["email", "currentAddress"]);
+
+    await chooseDateOfBirth(page, validData.birthDate);
+    await uploadImage(page, validData.picture);
+    await selectLocation(page, validData.stateCity);
+
+    await submitForm(page);
+
+    await invalidHighlight(page, formSelectors.firstName);
+    await invalidHighlight(page, formSelectors.lastName);
+    await invalidHighlight(page, formSelectors.mobile);
+  });
+
+  test("filling in only obligatory data", async ({ page }) => {
+    await fillPartialTextData(page, validData, [
+      "firstName",
+      "lastName",
+      "email",
+      "mobile",
+    ]);
+    await chooseOption(page, "gender", [validData.gender]);
+
+    await submitForm(page);
+
+    await showModal(page);
+    await expectTableOneValue(
+      page,
+      "Student Name",
+      `${validData.firstName} ${validData.lastName}`
+    );
+    await expectTableOneValue(page, "Student Email", `${validData.email}`);
+    await expectTableOneValue(page, "Mobile", `${validData.mobile}`);
+    // отображается дефолтная дата рождения (дата заполнения)
+
+    await closeModal(page);
+  });
+
+  test("filling in valid data and invalid email", async ({ page }) => {
+    await fillAllTextData(page, TestDataInvalEmail);
+
+    await chooseOption(page, "gender", [TestDataInvalEmail.gender]);
+    // await chooseOption(page, "subject", [...TestDataInvalEmail.subjects]);
+    // await chooseOption(page, "hobbies", [...TestDataInvalEmail.hobbies]);
+
+    await chooseDateOfBirth(page, TestDataInvalEmail.birthDate);
+    await uploadImage(page, TestDataInvalEmail.picture);
+    await selectLocation(page, TestDataInvalEmail.stateCity);
+
+    await submitForm(page);
+
+    await invalidHighlight(page, formSelectors.email);
+    // написать проверку на остальные поля с псевдокласами :valid
+    // Current Address, Hobbies, Mobile(10 Digits), Date of Birth, Name, Gender validHighlight()
+  });
+
+  test("fill all valid data without name", async ({ page }) => {
+    await fillPartialTextData(page, validData, [
+      "lastName",
+      "email",
+      "mobile",
+      "currentAddress",
+    ]);
+
+    await chooseOption(page, "gender", [validData.gender]);
+    // await chooseOption(page, "subject", [...validData.subjects]);
+    // await chooseOption(page, "hobbies", [...validData.hobbies]);
+
+    await chooseDateOfBirth(page, validData.birthDate);
+    await uploadImage(page, validData.picture);
+    await selectLocation(page, validData.stateCity);
+
+    await submitForm(page);
+
+    await invalidHighlight(page, formSelectors.firstName);
+    // написать проверку на остальные поля с псевдокласами :valid и :invalid
   });
 });
--- form_task/types/types.ts
+++ form_task/types/types.ts
@@ -1,51 +1,68 @@
-export type Gender = 'Male' | 'Female' | 'Other';
-export type Hobby = 'Sports' | 'Reading' | 'Music';
+export type Gender = "Male" | "Female" | "Other";
+export type Hobby = "Sports" | "Reading" | "Music";
 
 export interface userData {
-    firstName: string;
-    lastName: string;
-    gender: Gender;
-    email: string;
-    mobile: string;
-    birthDate: string; // формат YYYY-MM-DD
-    subjects: string[];
-    hobbies: Hobby[];
-    picture: string; // путь к локальному файлу
-    currentAddress: string;
-    stateCity: {
-        state: string;
-        city: string;
-    };
+  firstName: string;
+  lastName: string;
+  gender: Gender;
+  email: string;
+  mobile: string;
+  birthDate: string; // формат YYYY-MM-DD
+  subjects: string[];
+  hobbies: Hobby[];
+  picture: string; // путь к локальному файлу
+  currentAddress: string;
+  stateCity: {
+    state: string;
+    city: string;
+  };
 }
 
 export const validData: userData = {
-    firstName: 'Alex',
-    lastName: 'Ivanov',
-    email: 'ivanov@mail.com',
-    gender: 'Male',
-    mobile: '2912345678',
-    birthDate: '2000-05-15',
-    subjects: ['Math', 'Physics', 'English'],
-    hobbies: ['Sports', 'Reading'],
-    picture: './images/image.jpg',
-    currentAddress: 'ул. Ленина, д. 10, Минск',
-    stateCity: {
-        state: 'NCR',
-        city: 'Delhi',
-    },
+  firstName: "Alex",
+  lastName: "Ivanov",
+  email: "ivanov@mail.com",
+  gender: "Male",
+  mobile: "2912345678",
+  birthDate: "2000-05-15",
+  subjects: ["Math", "Physics", "English"],
+  hobbies: ["Sports", "Reading"],
+  picture: "image.jpg",
+  currentAddress: "ул. Ленина, д. 10, Минск",
+  stateCity: {
+    state: "NCR",
+    city: "Delhi",
+  },
+};
+
+export const TestDataInvalEmail: userData = {
+  firstName: "Ivan",
+  lastName: "Sidorov",
+  gender: "Male",
+  email: "ivan.sidorov", // неверный формат email
+  mobile: "9876543210",
+  birthDate: "1985-03-20",
+  subjects: ["English", "Economics"],
+  hobbies: ["Sports", "Music"],
+  picture: "image.jpg",
+  currentAddress: "ул. Пушкина, д. 12, Москва",
+  stateCity: {
+    state: "NCR",
+    city: "Delhi",
+  },
 };
 
 export const formSelectors = {
-    firstName: '#firstName',
-    lastName: '#lastName',
-    email: '#userEmail',
-    mobile: '#userNumber',
-    currentAddress: '#currentAddress',
-    gender: (value: string) => `input[name="gender"][value="${value}"]`,
-    birthDate: '#dateOfBirthInput',
-    subjectsInput: '#subjectsInput',
-    hobbies: (label: string) => `label:has-text("${label}")`,
-    picture: '#uploadPicture',
-    state: '#state',
-    city: '#city',
+  firstName: "#firstName",
+  lastName: "#lastName",
+  email: "#userEmail",
+  mobile: "#userNumber",
+  currentAddress: "#currentAddress",
+  gender: (value: string) => `input[name="gender"][value="${value}"]`,
+  birthDate: "#dateOfBirthInput",
+  subjectsInput: "#subjectsInput",
+  hobbies: (label: string) => `label:has-text("${label}")`,
+  picture: "#uploadPicture",
+  state: "#state",
+  city: "#city",
 };
